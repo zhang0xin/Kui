e7c359db0eb1fc50eed819fd9f22793811755d58
zhangyanxin <zhang0xin@gmail.com>
2019-04-26 12:21:48 +0800

    new objects table structure

--- Kui.Core/Resource/Persistence/Sqlite/SqliteDataAccesser.cs
+++ Kui.Core/Resource/Persistence/Sqlite/SqliteDataAccesser.cs
@@ -8,6 +8,7 @@ using System.Data.SQLite;
 using System.Reflection;
 using Dapper;
 using Kui.Core.Resource.Node;
+using System.Text;
 
 namespace Kui.Core.Resource.Persistence.Sqlite 
 {
@@ -27,35 +28,17 @@ namespace Kui.Core.Resource.Persistence.Sqlite
         {
             using (var conn = CreateConnection())
             {
-                var sql = "select * from base_node where path = @path";
+                var table = GetObjectsTableSql<T>();
+                var sql = $"select * from ({table}) t where t.path = @path";
                 object param = new {path};
                 if (path.EndsWith(PathSeparator))
                 {
-                    sql = "select * from base_node where path like @path1 and path not like @path2";
+                    sql = "select * from ({table}) t where t.path like @path1 and t.path not like @path2";
                     param = new{ path1=$"{path}_%", path2="{path}_%/%"};
                 }
                 sql += " order by update_time desc, create_time desc";
-                var nodes = conn.Query<T>( sql, param);
-                var keys = nodes.Select(n=> n.Key);
 
-                var dataProps = conn.Query(
-                    "select * from node_props where base_node_key in @keys", new {keys});
-                var dicProps = new Dictionary<string, object>();
-                foreach(var prop in dataProps)
-                {
-                    dicProps.Add($"{prop.base_node_key}:{prop.name}", prop.value);
-                }
-                foreach(var node in nodes)
-                {
-                    var type = node.GetType();
-                    var props = type.GetProperties(
-                        BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
-                    foreach(var prop in props)
-                    {
-                        prop.SetValue(node, dicProps[$"{node.Key}:{prop.Name}"]);
-                    }
-                }
-                return nodes;
+                return conn.Query<T>(sql, param);
             }
         }
 
@@ -68,22 +51,46 @@ namespace Kui.Core.Resource.Persistence.Sqlite
                 {
                     node.Key = IdentityGenerator.NewGuid();
                     node.CreateTime = DateTime.Now;
-                    conn.Execute(
-                        "insert into base_node (key, create_time, caption, description, path) "+
-                        "values (@key, @createTime, @caption, @description, @path)", node);
                 }
-                else
-                {
-                    conn.Execute(
-                        "update base_node set create_time=@createTime, caption=@caption, "+
-                        "description=@description, path=@path where key=@key", node);
-                }
+                SaveObject(node);
+            }
+        }
+        public void RemoveSiteNode(string path)
+        {
+            using(var conn = CreateConnection())
+            {
+                var pathmode = $"{path}%";
+                var keys = conn.Query<ulong>(
+                    "select ObjectId from Objects where Property='Path' and Value like @path",
+                    new { path = pathmode });
+                conn.Execute( "delete from Objects where ObjectId in @keys", new{keys});
+            }
+        }
+        public void RemoveSiteNode(ulong key)
+        {
+            DeleteObject(key);
+        }
 
-                var props = node.GetType().GetProperties(
-                    BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+        public void DeleteObject(ulong id)
+        {
+            using(var conn = CreateConnection())
+            {
+                conn.Execute("delete from Objects where ObjectId = @id;", new{id});
+            }
+        }
+        public void SaveObject(Object obj)
+        {
+            var type = obj.GetType();
+            var props = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            var idprop = props
+                .Where(p => p.GetCustomAttribute(typeof(IdentityAttribute)) != null)
+                .FirstOrDefault();
+            using (var conn = CreateConnection())
+            {
+                ulong objectId = (ulong)idprop.GetValue(obj);
                 var datas = conn.Query(
-                    "select name from node_props where base_node_key = @baseNodeKey", 
-                    new {baseNodeKey = node.Key});
+                    "select Property from Objects where ObjectId = @ObjectId", 
+                    new {ObjectId = objectId});
                 var dicActions = new Dictionary<string, string>();
 
                 foreach(var data in datas)
@@ -97,68 +104,53 @@ namespace Kui.Core.Resource.Persistence.Sqlite
                 }
                 foreach(var prop in props)
                 {
+                    if (prop == idprop) continue;
+                    
+                    var value = prop.GetValue(obj);
+                    var param = new
+                    {
+                        Id = IdentityGenerator.NewGuid(),
+                        ObjectId = objectId,
+                        Property = prop.Name,
+                        Value = value,
+                        Type = value.GetType().ToString() 
+                    };
                     if (dicActions[prop.Name] == "update")
                     {
-                        var value = prop.GetValue(node);
                         conn.Execute(
-                            "update node_props set value=@value, type=@type " +
-                            "where base_node_key=@baseNodeKey and name=@name",
-                            new
-                            {
-                                baseNodeKey = node.Key,
-                                name = prop.Name,
-                                value = value,
-                                type = value.GetType().ToString() 
-                            }
+                            "update Objects set Value=@Value, Type=@Type " +
+                            "where ObjectId=@ObjectId and Property=@Property",
+                            param
                         );
                     }
                     else if (dicActions[prop.Name] == "insert")
                     {
-                        var value = prop.GetValue(node);
                         conn.Execute(
-                            "insert into node_props (key, base_node_key, name, value, type) "+
-                            "values(@key, @baseNodeKey, @name, @value, @type)",
-                            new
-                            {
-                                key = IdentityGenerator.NewGuid(),
-                                baseNodeKey = node.Key,
-                                name = prop.Name,
-                                value = value,
-                                type = value.GetType().ToString() 
-                            }
+                            "insert into Objects (Id, ObjectId, Property, Value, Type) "+
+                            "values(@Id, @ObjectId, @Name, @Value, @Type)",
+                            param
                         );
                     }
                     else if (dicActions[prop.Name] == "delete")
                     {
-                        conn.Execute("delete from node_props where base_node_key=@baseNodeKey and name=@name",
-                            new { baseNodeKey = node.Key, name = prop.Name });
+                        conn.Execute(
+                            "delete from Objects where ObjectId=@ObjectId and Property=@Property",
+                            param);
                     }
                 }
             }
         }
-        public void RemoveSiteNode(string path)
+        string GetObjectsTableSql<T>()
         {
-            using(var conn = CreateConnection())
+            Type type = typeof(T);
+            var props = type.GetProperties(
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
+            var builder = new StringBuilder();
+            foreach(var prop in props)
             {
-                var pathmode = $"{path}%";
-                var nodes = conn.Query<ResourceNode>(
-                    "select * from base_node where path like @path " +
-                    "order by update_time desc, create_time desc", 
-                    new { path = pathmode });
-                var keys = nodes.Select(n=> n.Key);
-                conn.Execute(
-                    "delete from base_node where path like @path;"+
-                    "delete from node_props where base_node_key in @keys", 
-                    new{path = pathmode, keys});
+                builder.Append($", group_concat(case Property when '{prop.Name}' then Value else NULL end");
             }
-        }
-        public void RemoveSiteNode(ulong key)
-        {
-            using(var conn = CreateConnection())
-            {
-                conn.Execute("delete from base_node where key = @key;"+
-                    "delete from node_props where base_node_key = @key;", new{key});
-            }
+            return $" select ObjectId {builder} from Objects group by ObjectId ";
         }
 
         SQLiteConnection CreateConnection()
@@ -187,6 +179,14 @@ namespace Kui.Core.Resource.Persistence.Sqlite
                     value           text        not null,
                     type            text        not null
                 );
+                create table Objects
+                {
+                    Id          integer     primary key,
+                    ObjectId    integer     not null,
+                    Property    text        not null,
+                    Value       text,
+                    Type        text
+                };
             ";
             var connection = CreateConnection();
             connection.Execute(sql);
